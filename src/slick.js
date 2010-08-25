ff708c9aec112e84fd1cdabab20ab43c8312116c
frw <warnis@gmail.com>
2010-08-25 15:51:21 +0200

    updated readme and theme finally working

--- src/slick.js
+++ src/slick.js
@@ -2,3 +2,7 @@ var Slick = SC.AceTheme.extend({
   "classNames": ["slick", "normal"]
 });
 SC.Theme.register("slick", Slick);
+SC.Pane.prototype.theme = "slick"
+
+// Hack for rightclick in test_controls
+SC.CONTEXT_MENU_ENABLED = YES
